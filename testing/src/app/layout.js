e8668b925648b2bf91386d7e6a06f4bb9dee93bc
Jagrat Singh <jagratsingh9899@gmail.com>
2025-03-17 02:09:27 +0530

    header

--- testing/src/app/layout.js
+++ testing/src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,8 +22,11 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      ><div>
+        <Header />
+        <div className="h-[40px] w[100%]"></div>
         {children}
+      </div>
       </body>
     </html>
   );
---
+++ testing/src/components/header.js
@@ -0,0 +1,49 @@
+"use client";
+
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <header style={headerStyle}>
+      <h1 style={logoStyle}>🌾 AI Agriculture</h1>
+      <nav style={navStyle}>
+        <Link href="/" style={linkStyle}>Home</Link>
+        <Link href="/Features" style={linkStyle}>Features</Link>
+        <Link href="/dashboard" style={linkStyle}>Dashboard</Link>
+        <Link href="/about" style={linkStyle}>About</Link>
+      </nav>
+    </header>
+  );
+};
+
+const headerStyle = {
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+  padding: "10px 20px",
+  backgroundColor: "rgba(241, 245, 249, 0.8)",
+  borderBottom: "1px solid #ddd",
+  position: "fixed",
+  width: "100%",
+  backdropFilter: "blur(5px)",
+  zIndex: 50,
+};
+
+const logoStyle = {
+  fontSize: "24px",
+  fontWeight: "bold",
+  color: "#333",
+};
+
+const navStyle = {
+  display: "flex",
+  gap: "20px",
+};
+
+const linkStyle = {
+  textDecoration: "none",
+  color: "#333",
+  fontSize: "16px",
+};
+
+export default Header;
